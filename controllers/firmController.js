a14c2b6dfdf30cd7a9e379de24a6cd1bd3e0dbd2
Manasa Reddy <kandadimanasareddy12@gmail.com>
2024-08-12 18:15:03 +0530

    modified product datatype

--- controllers/firmController.js
+++ controllers/firmController.js
@@ -41,14 +41,16 @@ const addFirm = async (req, res) => {
 
     const savedFirm = await firm.save();
 
-    // const firmId = savedFirm._id
+    const firmId = savedFirm._id;
     // const vendorFirmName = savedFirm.firmName
 
     vendor.firm.push(savedFirm);
 
     await vendor.save();
 
-    return res.status(200).json({ message: "Firm Added successfully " });
+    return res
+      .status(200)
+      .json({ message: "Firm Added successfully ", firmId });
   } catch (error) {
     console.error(error);
     res.status(500).json("intenal server error");
